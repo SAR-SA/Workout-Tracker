b449e2080b0b991d0c126cdffb1585ee079ec083
Shawn A. R <sarsa.web@gmail.com>
2020-10-17 18:18:24 -0400

    html routes

--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,24 @@ const Workout = require("../models/workout.js");
 
 router.post("/api/workout", ({ body }, res) => {
     Workout.create(body)
-    .then(db)
-})
+        .then(dbWorkout => {
+            console.log("Workout is: ");
+            console.log(dbTransaction);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.get()
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const apiRoutes = require("./routes/api.js");
+const htmlRoutes = require("./routes/html-routes.js")
+
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,7 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
